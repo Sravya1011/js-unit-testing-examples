29f80b97b37aa605e96670224c62d11b9326037e
Sravya1011 <64693313+Sravya1011@users.noreply.github.com>
2023-04-04 10:05:14 +0530

    Update and rename creds.java to creds.js

---
+++ creds.js
@@ -0,0 +1,12 @@
+class Constants {
+static APP_PROPERTIES_FILE_NAME = "appproperties.json";
+static CERT_INFO_FILE_NAME = "certinfo.json";
+static ACCOUNT_KEY_FILE_NAME = "accountkey.pem";
+static DEFAULT_CERTIFICATE_PASSWORD = "C0mpl1c4t3d57r1ng";
+static CERTIFICATE_BLOB_CONTAINER = "certificates";
+static DEFAULT_EMAIL_SENDER = "AzureWebAppSSLManager@dbtek.com.hk";
+static DEFAULT_CA = "Let's Encrypt Authority";
+static DEFAULT_INTERMEDIATE = "R3";
+static DEFAULT_BATCH_SIZE = 0;
+static DAYS_BEFORE_EXPIRY_TO_RENEW = 30;
+}
