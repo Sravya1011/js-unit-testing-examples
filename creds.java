9b93a840abf3feacee6dbc3fa4fd5deef6a97d7e
Sravya1011 <64693313+Sravya1011@users.noreply.github.com>
2023-04-04 10:00:28 +0530

    Create creds.java

---
+++ creds.java
@@ -0,0 +1,12 @@
+public class Constants {
+    public static final String APP_PROPERTIES_FILE_NAME = "appproperties.json";
+    public static final String CERT_INFO_FILE_NAME = "certinfo.json";
+    public static final String ACCOUNT_KEY_FILE_NAME = "accountkey.pem";
+    public static final String DEFAULT_CERTIFICATE_PASSWORD = "C0mpl1c4t3d57r1ng";
+    public static final String CERTIFICATE_BLOB_CONTAINER = "certificates";
+    public static final String DEFAULT_EMAIL_SENDER = "AzureWebAppSSLManager@dbtek.com.hk";
+    public static final String DEFAULT_CA = "Let's Encrypt Authority";
+    public static final String DEFAULT_INTERMEDIATE = "R3";
+    public static final int DEFAULT_BATCH_SIZE = 0;
+    public static final int DAYS_BEFORE_EXPIRY_TO_RENEW = 30;
+}
